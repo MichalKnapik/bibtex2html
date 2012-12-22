58a1d0a99f64046b4c17d724074bbe43574028b3
michal.knapik <knapikgm@gmail.com>
2012-12-22 13:34:41 +0100

    Fast-coded version is ready

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 bibtex2html
 ===========
 
-Takes a .bib bibTeX file and produces html output
+Takes a *.bib bibTeX file and produces html output in a form of
+an unordered sorted list of publications. To link to the document,
+add uri = "...", entry in bibTeX. Fast-coded.
--- bibtex2html.py
+++ bibtex2html.py
@@ -12,12 +12,17 @@ if __name__ == "__main__":
     try:
         fetcher = pubFetcher()
         fetcher.loadPubs(src)
+
+        #an example use of fetcher
         print '<h1 class="journpub">Journal publications</h1><ul>'
         fetcher.publistHTML("ARTICLE")
         print "</ul>" 
+
         print '<h1 class="procpub">Conference publications</h1><ul>'
         fetcher.publistHTML("INPROCEEDINGS")
         print "</ul>" 
+        #------------------------
+
     except IOError as ioerr:
         print ioerr
     except BibException as biberr:
